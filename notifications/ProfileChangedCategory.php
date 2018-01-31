ff2b72a9d11a307b035d75c6e0036512017d9c0e
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-01-31 15:53:44 +0900

    Update ProfileChangedCategory.php

--- notifications/ProfileChangedCategory.php
+++ notifications/ProfileChangedCategory.php
@@ -2,13 +2,18 @@
 
 /**
  * @link https://www.humhub.org/
- * @copyright Copyright (c) 2016 HumHub GmbH & Co. KG
+ * @copyright Copyright (c) 2018 HumHub GmbH & Co. KG
  * @license https://www.humhub.com/licences
  */
 
 namespace humhub\modules\profilenotify\notifications;
 
 use Yii;
+use humhub\modules\user\models\User;
+use humhub\modules\notification\targets\BaseTarget;
+use humhub\modules\notification\targets\MailTarget;
+use humhub\modules\notification\targets\WebTarget;
+use humhub\modules\notification\targets\MobileTarget;
 use humhub\modules\notification\components\NotificationCategory;
 use humhub\modules\notification\components\NotificationTarget;
 
@@ -24,6 +29,11 @@ class ProfileChangedCategory extends NotificationCategory
      * @var string
      */
     public $id = 'profilenotify';
+
+    /**
+     * @var int used to sort categories
+     */
+    public $sortOrder = PHP_INT_MAX;
 
     /**
      * @inheritdoc
@@ -44,16 +54,52 @@ class ProfileChangedCategory extends NotificationCategory
     /**
      * @inheritdoc
      */
-    public function getDefaultSetting(NotificationTarget $target)
+    public function getDefaultSetting(BaseTarget $target)
     {
-        if ($target->id === \humhub\modules\notification\components\MailNotificationTarget::getId()) {
+        if ($target->id === MailNotificationTarget::getId()) {
             return true;
-        } else if ($target->id === \humhub\modules\notification\components\WebNotificationTarget::getId()) {
+        } elseif ($target->id === WebNotificationTarget::getId()) {
             return true;
-        } else if ($target->id === \humhub\modules\notification\components\MobileNotificationTarget::getId()) {
+        } elseif ($target->id === MobileNotificationTarget::getId()) {
             return true;
         }
 
         return $target->defaultSetting;
+    }
+
+    /**
+     * Returns an array of target ids, which are not editable.
+     * 
+     * @param BaseTarget $target
+     */
+    public function getFixedSettings()
+    {
+        return [];
+    }
+
+    /**
+     * Checks if the given notification target is fixed for this category.
+     * 
+     * @param type $target
+     * @return type
+     */
+    public function isFixedSetting(BaseTarget $target)
+    {
+        return in_array($target->id, $this->getFixedSettings());
+    }
+
+    /**
+     * Determines if this category is visible for the given $user.
+     * This can be used if a category is only visible for users with certian permissions.
+     * 
+     * Note if no user is given this function should return true in most cases, otherwise this
+     * category won't be visible in the global notification settings.
+     * 
+     * @param User $user
+     * @return boolean
+     */
+    public function isVisible(User $user = null)
+    {
+        return true;
     }
 }
